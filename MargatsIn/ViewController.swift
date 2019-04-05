4dcc00f68718b04d6e8a4c865f9db22e7fe67191
Tom <melooh42@gmail.com>
2019-04-05 12:40:48 +0900

    Initial Commit

---
+++ MargatsIn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MargatsIn
+//
+//  Created by 板垣智也 on 2019/04/05.
+//  Copyright © 2019 板垣智也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
