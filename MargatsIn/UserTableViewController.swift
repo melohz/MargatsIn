0c3edbdf1990ffa89dcafb8167b820fbf5475ebd
Tom <melooh42@gmail.com>
2019-04-14 16:19:16 +0900

    Refactoring if let statement

--- MargatsIn/UserTableViewController.swift
+++ MargatsIn/UserTableViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import Parse
 
 class UserTableViewController: UITableViewController {
-
+    
     var usernames = [""]
     var objectIds = [""]
     var isFollowing = ["" : false]
@@ -17,9 +17,9 @@ class UserTableViewController: UITableViewController {
     var refresher: UIRefreshControl = UIRefreshControl()
     
     @IBAction func logoutUser(_ sender: Any) {
-
+        
         PFUser.logOut()
-
+        
         self.performSegue(withIdentifier: "logoutSegue", sender: self)
         
     }
@@ -40,38 +40,36 @@ class UserTableViewController: UITableViewController {
                 
                 for object in users {
                     if let user = object as? PFUser {
-                        if let username = user.username {
-                            if let objectId = user.objectId {
-                                let usernameArray = username.components(separatedBy: "@")
-                                self.usernames.append(usernameArray[0])
-                                self.objectIds.append(objectId)
-                                
-                                let query = PFQuery(className: "Following")
-                                
-                                query.whereKey("follower", equalTo: PFUser.current()?.objectId)
-                                query.whereKey("following", equalTo: objectId)
-                                
-                                query.findObjectsInBackground(block: { (objects, error) in
-                                    if let objects = objects {
-                                        if objects.count > 0 {
-                                            
-                                            self.isFollowing[objectId] = true
-                                            
-                                        } else {
-                                            
-                                            self.isFollowing[objectId] = false
-                                            
-                                        }
+                        if let username = user.username, let objectId = user.objectId {
+                            let usernameArray = username.components(separatedBy: "@")
+                            self.usernames.append(usernameArray[0])
+                            self.objectIds.append(objectId)
+                            
+                            let query = PFQuery(className: "Following")
+                            
+                            query.whereKey("follower", equalTo: PFUser.current()?.objectId)
+                            query.whereKey("following", equalTo: objectId)
+                            
+                            query.findObjectsInBackground(block: { (objects, error) in
+                                if let objects = objects {
+                                    if objects.count > 0 {
                                         
-                                        if self.usernames.count == self.isFollowing.count {
+                                        self.isFollowing[objectId] = true
                                         
-                                            self.tableView.reloadData()
-                                            
-                                            self.refresher.endRefreshing()
-                                        }
+                                    } else {
+                                        
+                                        self.isFollowing[objectId] = false
+                                        
                                     }
-                                })
-                            }
+                                    
+                                    if self.usernames.count == self.isFollowing.count {
+                                        
+                                        self.tableView.reloadData()
+                                        
+                                        self.refresher.endRefreshing()
+                                    }
+                                }
+                            })
                         }
                     }
                 }
@@ -82,32 +80,32 @@ class UserTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         updateTable()
-
+        
         refresher.attributedTitle = NSAttributedString(string: "Pull to refresh")
         
         refresher.addTarget(self, action: #selector(UserTableViewController.updateTable), for: UIControl.Event.valueChanged)
         
         tableView.addSubview(refresher)
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return usernames.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        
         cell.textLabel?.text = usernames[indexPath.row]
         
         if let followsBoolean = isFollowing[objectIds[indexPath.row]]{
@@ -118,7 +116,7 @@ class UserTableViewController: UITableViewController {
         
         return cell
     }
- 
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath)
         
@@ -150,7 +148,7 @@ class UserTableViewController: UITableViewController {
             } else {
                 
                 isFollowing[objectIds[indexPath.row]] = true
-
+                
                 cell?.accessoryType = UITableViewCell.AccessoryType.checkmark
                 
                 let following = PFObject(className: "Following")
@@ -161,50 +159,50 @@ class UserTableViewController: UITableViewController {
             }
         }
     }
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
